d28cc70ffbd1a09c20ff2a980b380d101a8e5d04
Ana-Bogdan <ana2004bogdan@gmail.com>
2025-08-13 15:47:40 +0300

    fix: add error handling for missing products in cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -7,16 +7,20 @@ class CartController < ApplicationController
   end
 
   def create
-    product = Product.find(params[:product_id])
-    cart_item = @cart.cart_items.find_by(product: product)
+    begin
+      product = Product.find(params[:product_id])
+      cart_item = @cart.cart_items.find_by(product: product)
 
-    if cart_item
-      cart_item.update(quantity: cart_item.quantity + 1)
-    else
-      @cart.cart_items.create(product: product, quantity: 1)
+      if cart_item
+        cart_item.update(quantity: cart_item.quantity + 1)
+      else
+        @cart.cart_items.create(product: product, quantity: 1)
+      end
+
+      redirect_back(fallback_location: root_path, notice: "#{product.name} added to cart!")
+    rescue ActiveRecord::RecordNotFound
+      redirect_back(fallback_location: root_path, alert: "Product not found.")
     end
-
-    redirect_back(fallback_location: root_path, notice: "#{product.name} added to cart!")
   end
 
   def destroy
