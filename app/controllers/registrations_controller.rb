a6eef8ff34db2ca3ff13dd87af3edd6f4e94f9b4
Ana-Bogdan <ana2004bogdan@gmail.com>
2025-08-13 15:14:20 +0300

    feat: personalize flash messages with user names

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,7 +11,7 @@ class RegistrationsController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path, notice: "Account created successfully!"
+      redirect_to root_path, notice: "Welcome, #{@user.name}!"
     else
       flash.now[:alert] = "Registration failed. Please check your information."
       render :new
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_path, notice: "Successfully logged in!"
+      redirect_to root_path, notice: "Welcome back, #{user.name}!"
     else
       flash.now[:alert] = "Invalid email or password"
       render :new
